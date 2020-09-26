acc1aaa82c303c6787e7a915ce21e2513c99d2aa
Luis Carlos Charris Molina <luis.carlos.charris@gmail.com>
2020-09-25 20:53:33 -0500

    Update sqlConstant.js
    Corrección de pais

--- config/sqlConstant.js
+++ config/sqlConstant.js
@@ -1,7 +1,7 @@
 const SQL = {
     SCHEMA: {
-        CO: "INVOICEVE",
-        VE: "INVOICE"
+        CO: "INVOICE",
+        VE: "INVOICECO"
     },
     INSERT_INVOICE_HEAD:
         "INSERT INTO SCHEMA.INVOICE_HEAD " +
